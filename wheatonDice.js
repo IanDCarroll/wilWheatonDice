4b1b203a0e0c76187f7a1db33ea9db986016e68e
Ian Carroll <aidanogain@gmail.com>
2016-04-13 19:36:12 -0700

    add percent die & mixed rolls

--- wheatonDice.js
+++ wheatonDice.js
@@ -67,32 +67,113 @@ function rollOut(diceNumber, diceSides, rolls, rollTotal) {
 
 //input functions.
 //uses public variables to access physics() function.
+//ATM roll is a dependency for special roll functions, may need to correct.
 function roll(diceNumber, diceSides) {
     return physics(diceNumber, diceSides);
 }
 
-//Special kinds of D&D rolls, Advantage, Disadvantage, Inspiration and mixed rolls
-function rollAdvantage () {
+/*Mixed dice rolls. There are 6 Standard dice in D&D.
+d4, d6, d8, d10, d12, and d20. It would be quite unusual for all these 
+types of dice to be rolled at the same time, but just in case, 
+rollMix will be able to handle up to 6 different kinds of dice rolls. */
+function rollMix(dNum1, dSid1, dNum2, dSid2, dNum3, dSid3, 
+		 dNum4, dSid4, dNum5, dSid5, dNum6, dSid6) {
+    var diceMix = [],
+	mixTotal = 0;
+	diceRold = [];
+
+    roll(dNum1, dSid1);
+    diceRold.push(dNum1 + " d" + dSid1);
+    for (var i = 0; i < displayRolls.length; i++) {
+	diceMix.push(displayRolls[i]);
+    }
+
+    if (dNum2 > 0) {
+	roll(dNum2, dSid2);
+	diceRold.push(dNum2 + " d" + dSid2);
+	for (var i = 0; i < displayRolls.length; i++) {
+		diceMix.push(displayRolls[i]);
+	}
+    }
+    if (dNum3 > 0) {
+	roll(dNum3, dSid3);
+	diceRold.push(dNum3 + " d" + dSid3);
+	for (var i = 0; i < displayRolls.length; i++) {
+		diceMix.push(displayRolls[i]);
+	}
+    }
+    if (dNum4 > 0) {
+	roll(dNum4, dSid4);
+	diceRold.push(dNum4 + " d" + dSid4);
+	for (var i = 0; i < displayRolls.length; i++) {
+		diceMix.push(displayRolls[i]);
+	}
+    }
+    if (dNum5 > 0) {
+	roll(dNum5, dSid5);
+	diceRold.push(dNum5 + " d" + dSid5);
+	for (var i = 0; i < displayRolls.length; i++) {
+		diceMix.push(displayRolls[i]);
+	}
+    }
+    if (dNum6 > 0) {
+	roll(dNum6, dSid6);
+	diceRold.push(dNum6 + " d" + dSid6);
+	for (var i = 0; i < displayRolls.length; i++) {
+		diceMix.push(displayRolls[i]);
+	}
+    }
+
+    mixTotal = diceMix.reduce(function(a ,b) {
+	return a + b;
+    });
+
+    var dispString = (diceRold.join(", ") + ": " 
+	+ diceMix.join(", ") + " Total: " + mixTotal);
+
+    return dispString;
+}
+
+//Special kinds of D&D rolls, Advantage, Disadvantage, Percent, Inspiration and mixed rolls
+function rollAdvantage() {
     roll(2, 20);
     displayTotal = Math.max(displayRolls[0], displayRolls[1]);
-    return (displayRolls.join(" and ") + ". Result: " + displayTotal);
+    return (displayRolls.join(" & ") + ". Result: " + displayTotal);
 }
 
-function rollDisadvantage () {
+function rollDisadvantage() {
     roll(2, 20);
     displayTotal = Math.min(displayRolls[0], displayRolls[1]);
-    return (displayRolls.join(" and ") + ". Result: " + displayTotal);
+    return (displayRolls.join(" & ") + ". Result: " + displayTotal);
 }
 
-//Accesses the last most recent roll, check if it is a d20 roll, then add itself to that roll. 
+function rollPercent() {
+    roll(2, 10);
+
+    dicePercent = 0;
+
+    dicePercent = ((displayRolls[0] % 10) * 10) + (displayRolls[1] % 10);
+
+    if (dicePercent < 1) {
+	dicePercent = 100;
+    }
+
+    displayTotal = dicePercent;
+
+    return (displayRolls.join(" & ") + " %Score: " + displayTotal); 
+}
+
+//Accesses the last most recent roll, check if it is a d20 roll, 
+//then add itself to that roll. 
 //Handles Advantage, & Disadvantage rolls as well.
-function rollInspiration () {
+function rollInspiration() {
     var lastRoll = 0;
 
     if (displaySides === 20) {
 	lastRoll = displayTotal;
 	roll(1, 10);
-	return ("+" + displayTotal + " to " + lastRoll + ": " + (lastRoll + displayTotal));
+	return ("+" + displayTotal + " to " + lastRoll + ": " 
+		+ (lastRoll + displayTotal));
     } else {
 	return ("You can only add an inspiration die to a d20 roll.");
     }
@@ -127,3 +208,11 @@ console.log(rollManual(2, 12, [5, 2]));
 console.log(rollInspiration());
 console.log(rollManual(1, 20, [20]));
 console.log(rollInspiration());
+console.log(rollMix(2, 12));
+console.log(rollMix(2, 12, 3, 10));
+console.log(rollPercent());
+console.log(rollPercent());
+console.log(rollPercent());
+console.log(rollPercent());
+console.log(rollPercent());
+
