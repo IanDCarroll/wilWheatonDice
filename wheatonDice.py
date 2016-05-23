06f741143fddcd94a1ae986cf53514325ac20fe2
IanDCarroll <ian.d.carroll@gmail.com>
2016-05-23 09:59:02 -0700

    declare initial functions and framework setup

--- wheatonDice.py
+++ wheatonDice.py
@@ -1,1 +1,47 @@
 #python port of wheatonDice
+
+#import neccesary libraries
+from datetime import datetime
+
+#public variables
+diNumbr = 0
+diSides = 0
+diRolls = []
+diTotal = 0
+wheaton = ""
+#placeholder for Database pusher
+rollDB = []
+
+#internal functions
+def physics(dNum, dSid)
+    rolls = []
+    diSides = dSid
+    diNumbr = dNum
+
+def timeStamp()
+    #todo: research and apply python datetime
+def rollDBizer()
+
+def wheatonize()
+
+def rollOut()
+    print "%s d%s: %s Total: %s %s" % (diNumbr, diSides, diRolls, diTotal, wheaton)
+
+#calling functions
+def roll(dNum, dSid)
+    physics(dNum, dSid)
+    rollDBizer()
+    wheatonize()
+    print rollOut()
+
+def rollMix
+
+def rollAdvantage()
+
+def rollDisadvantage()
+
+def rollPercent()
+
+def rollInspiration()
+
+def rollManual()
