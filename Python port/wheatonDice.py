daf2496b456e460a40bcf21f8bd10fa36fe24b91
IanDCarroll <ian.d.carroll@gmail.com>
2016-05-26 11:03:28 -0700

    add timestamp function

---
+++ Python port/wheatonDice.py
@@ -0,0 +1,50 @@
+#python port of wheatonDice
+
+#import neccesary libraries
+from datetime import datetime
+
+#public variables
+diNumbr = 0
+diSides = 0
+diRolls = []
+diTotal = 0
+wheaton = ""
+#placeholder for Database pusher
+rollDB = []
+
+#internal functions
+def physics(dNum, dSid)
+    rolls = []
+    diSides = dSid
+    diNumbr = dNum
+
+def timeStamp()
+    #literal from JS. Is this function even necessary in python?
+    now = datetime.now()
+    return now
+
+def rollDBizer()
+
+def wheatonize()
+
+def rollOut()
+    print "%s d%s: %s Total: %s %s" % (diNumbr, diSides, diRolls, diTotal, wheaton)
+
+#calling functions
+def roll(dNum, dSid)
+    physics(dNum, dSid)
+    rollDBizer()
+    wheatonize()
+    print rollOut()
+
+def rollMix
+
+def rollAdvantage()
+
+def rollDisadvantage()
+
+def rollPercent()
+
+def rollInspiration()
+
+def rollManual()
