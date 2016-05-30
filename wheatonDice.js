30c84ca12e2f57af203b080f3e86a2a62ef2425c
IanDCarroll <ian.d.carroll@gmail.com>
2016-05-29 19:56:00 -0700

    change public vars to a dice object

--- wheatonDice.js
+++ wheatonDice.js
@@ -1,4 +1,4 @@
-/* wheatonDice is an opensource project to study the legendary dice curse of Wil Wheaton
+/* di.whtnDice is an opensource project to study the legendary dice curse of Wil Wheaton
  * and get to the bottom of bad juju. At the moment, it's just a dice roller, but later 
  * functions will include a rolls database that can track any sinking feeling that the kind of 
  * cheetos you're eating is influencing your rolls. Also v.1.0 is aiming at 5th edition D&D rules
@@ -7,13 +7,13 @@
  * working. Much to be built! -Ian
  */
 
-//public variables
-var diNumbr,
-    diSides,
-    diRolls,
-    diTotal,
-    wheaton,
-    diInsan = 'Sanity Error!';
+//the dice object
+var di = {  nmbr:undefined,
+    	    sids:undefined,
+    	    rols:undefined,
+    	    totl:undefined,
+    	    whtn:undefined,
+    	    insn:'Sanity Error!' };
 
 //saved rolls go here so they can be accessed and analyzed (unitl a proper DB is made)
 //currnet version forgets after the battery of console.log()s are made. Need a proper DB, 
@@ -29,32 +29,32 @@ function rollSanity() {
 	return isNaN(value)?!1:(x=parseFloat(value),(0|x)===x);
     }
 
-    if (diNumbr === undefined) {
-	diInsan = 'The outcome of your undefined number of dice\n'+
+    if (di.nmbr === undefined) {
+	di.insn = 'The outcome of your undefined number of dice\n'+
 		'with between zero and infinity number of sides each\n'+
 		'rolls unobserved in a mixed state of quantum flux\n'+
 		'forever both Percy and Wheaton, like that poor cat.\n'+ 
 		'Thus it remains with the Tao that cannot be told.\n\n'+
 		'Please define the number of dice and their sides\n'+
 		'if you prefer your rolls collapsed, measurable and pure.';
-    } else if (diNumbr === 0) {
-	diInsan = '    If zero dice are thrown in simulation,\n'+
+    } else if (di.nmbr === 0) {
+	di.insn = '    If zero dice are thrown in simulation,\n'+
 		'Even if someone is there to observe the outcome -\n'+
 		'    Do they still roll poorly for Wil Wheaton?';
-    } else if (diSides === undefined) {
-	diSides = diNumbr;
-	diNumbr = 1;
-	diInsan = undefined;
-    } else if (diSides === 0) {
-	diInsan = 'Zero sides?! Is it even there?!';
-    } else if (diSides === 1) {
-	diInsan = 'Ok, Escher-trippiness of a one-sided dice aside,\n'+
+    } else if (di.sids === undefined) {
+	di.sids = di.nmbr;
+	di.nmbr = 1;
+	di.insn = undefined;
+    } else if (di.sids === 0) {
+	di.insn = 'Zero sides?! Is it even there?!';
+    } else if (di.sids === 1) {
+	di.insn = 'Ok, Escher-trippiness of a one-sided dice aside,\n'+
 		'Exactly how did you not know the outcome of this roll?';
-    } else if (!(isInt(diNumbr)) || !(isInt(diNumbr))) {
-	diInsan = 'Please make sure you entered integers\n'+
+    } else if (!(isInt(di.nmbr)) || !(isInt(di.sids))) {
+	di.insn = 'Please make sure you entered integers\n'+
 	      'for the number of dice and how many sides they have'
     } else { 
-	diInsan = undefined; 
+	di.insn = undefined; 
     }
 }
 
@@ -66,18 +66,18 @@ function physics() {
     var rolls = [];
 
     //rolls each die separately then pushes to "rolls" array.
-    for (var i = 0; i < diNumbr; i++) {	
-	    rolls.push(Math.ceil( Math.random() * diSides ));     
+    for (var i = 0; i < di.nmbr; i++) {	
+	    rolls.push(Math.ceil( Math.random() * di.sids ));     
     }
 
-    diRolls = rolls;
+    di.rols = rolls;
 
     //folds the array into a neat total
     var totally = rolls.reduce(function(a ,b) {
 	return a + b;
     });
 
-    diTotal = totally;
+    di.totl = totally;
 }
 
 //timeStamp() acts as a serial number for the roll in rollDB.
@@ -90,46 +90,46 @@ function timeStamp() {
 function rollDBizer() {
     var rollEntry = [],
 
-    rollEntry = [timeStamp(), diNumbr, diSides, diRolls, diTotal];
+    rollEntry = [timeStamp(), di.nmbr, di.sids, di.rols, di.totl];
 
     rollDB.push(rollEntry);
 }
 
-//Separate wheatonizer, so that code is easy to delete.
+//Separate di.whtnizer, so that code is easy to delete.
 //make it so every lowest possible roll that's rolled displays "Wheaton!" 
     //instead of the number
 function wheatonize() {
-    if (diTotal === diNumbr) {
-	wheaton = "Wheaton!";
-    } else if (diTotal < (0.33 * (diNumbr * diSides))){
-	wheaton = "lesser Wheaton.";
-    } else if (diTotal === (diNumbr * diSides)) {
-	wheaton = "Percy!";
-    } else if (diTotal > (0.66 * (diNumbr * diSides))) {
-	wheaton = "lesser Percy.";
+    if (di.totl === di.nmbr) {
+	di.whtn = "Wheaton!";
+    } else if (di.totl < (0.33 * (di.nmbr * di.sids))){
+	di.whtn = "lesser Wheaton.";
+    } else if (di.totl === (di.nmbr * di.sids)) {
+	di.whtn = "Percy!";
+    } else if (di.totl > (0.66 * (di.nmbr * di.sids))) {
+	di.whtn = "lesser Percy.";
     } else {
-	wheaton = "";
+	di.whtn = "";
     }
 }
 	
 //returns a neat string that shows what you rolled, how each roll went, and the total.
 //This will probably be numberfied and separated out when the actual UI gets built
 function rollOut() {
-    if (diNumbr === 1) {
-	return ("1 d" + diSides + ": " + diTotal + " " + wheaton);
+    if (di.nmbr === 1) {
+	return ("1 d" + di.sids + ": " + di.totl + " " + di.whtn);
     } else { 
-	return (diNumbr + " d" + diSides + ": " + diRolls.join(", ") + 
-		" Total: " + diTotal + " " + wheaton);}
+	return (di.nmbr + " d" + di.sids + ": " + di.rols.join(", ") + 
+		" Total: " + di.totl + " " + di.whtn);}
 }
 
 //input functions.
 //uses public variables to access physics() function.
 function roll(dNum, dSid) {
-    diNumbr = dNum;
-    diSides = dSid;
+    di.nmbr = dNum;
+    di.sids = dSid;
 
     rollSanity();
-    if (diInsan) { return diInsan;
+    if (di.insn) { return di.insn;
     } else {
 
 	physics();
@@ -177,17 +177,17 @@ function rollMix(dNum1, dSid1, dNum2, dSid2, dNum3, dSid3,
     }
 
     function theRoll(dNum, dSid) {
-	diNumbr = dNum;
-	disides = dSid;
+	di.nmbr = dNum;
+	di.sids = dSid;
 	rollSanity()
-	if (diInsan) { 
-	    return diInsan;
+	if (di.insn) { 
+	    return di.insn;
 	} else {
 	    physics();
 	    rollDBizer();
    	    allDice.push(dNum + " d" + dSid);
-	    for (var i = 0; i < diRolls.length; i++) {
-		allRolls.push(diRolls[i]);
+	    for (var i = 0; i < di.rols.length; i++) {
+		allRolls.push(di.rols[i]);
 	    }
 	}
     }
@@ -215,57 +215,57 @@ function rollMix(dNum1, dSid1, dNum2, dSid2, dNum3, dSid3,
 
 //Special kinds of D&D rolls, Advantage, Disadvantage, Percent, Inspiration and mixed rolls.
 function rollAdvantage() {
-    diNumbr = 2;
-    diSides = 20;
+    di.nmbr = 2;
+    di.sids = 20;
     physics();
-    diNumbr = 1;
-    diTotal = Math.max(diRolls[0], diRolls[1]);
+    di.nmbr = 1;
+    di.totl = Math.max(di.rols[0], di.rols[1]);
     wheatonize();
-    diNumbr = "A";
+    di.nmbr = "A";
     rollDBizer();
-    return (diRolls.join(" & ") + ". Result: " + diTotal + " " + wheaton);
+    return (di.rols.join(" & ") + ". Result: " + di.totl + " " + di.whtn);
 }
 
 function rollDisadvantage() {
-    diNumbr = 2;
+    di.nmbr = 2;
     disides = 20;
     physics();
-    diNumbr = 1;
-    diTotal = Math.min(diRolls[0], diRolls[1]);
+    di.nmbr = 1;
+    di.totl = Math.min(di.rols[0], di.rols[1]);
     wheatonize();
-    diNumbr = "D";
+    di.nmbr = "D";
     rollDBizer();
-    return (diRolls.join(" & ") + ". Result: " + diTotal + " " + wheaton);
+    return (di.rols.join(" & ") + ". Result: " + di.totl + " " + di.whtn);
 }
 
 function rollPercent() {
     var rollTime = new Date();
-    diNumbr = 2;
-    diSides = 10;
+    di.nmbr = 2;
+    di.sids = 10;
     physics();
-    diNumbr = 1;
-    diSides = 100;
-    diTotal = ((diRolls[0] % 10) * 10) + (diRolls[1] % 10);
-    if (diTotal === 0) diTotal = 100;
+    di.nmbr = 1;
+    di.sids = 100;
+    di.totl = ((di.rols[0] % 10) * 10) + (di.rols[1] % 10);
+    if (di.totl === 0) di.totl = 100;
     wheatonize();
-    diNumbr = "%";
+    di.nmbr = "%";
     rollDBizer();
-    return (diRolls.join(" & ") + " %Score: " + diTotal + " " + wheaton); 
+    return (di.rols.join(" & ") + " %Score: " + di.totl + " " + di.whtn); 
 }
 
 //Accesses the last most recent roll, check if it is a d20 roll,
-    //May need to access rollDB instead of diTotal when DB goes up. 
+    //May need to access rollDB instead of di.totl when DB goes up. 
 //then add itself to that roll. 
 //Handles Advantage, & Disadvantage rolls as well.
 function rollInspiration() {
     var lastRoll = 0;
-    if (diSides === 20) {
-	lastRoll = diTotal;
-	diNumbr = 1;
-	disides = 10;
+    if (di.sids === 20) {
+	lastRoll = di.totl;
+	di.nmbr = 1;
+	di.sids = 10;
 	physics();
 	rollDBizer();
-	return ("+" + diTotal + " to " + lastRoll + ": " + (lastRoll + diTotal));
+	return ("+" + di.totl + " to " + lastRoll + ": " + (lastRoll + di.totl));
     } else {
 	return ("You can only add an inspiration die to a d20 roll.");
     }
@@ -273,13 +273,11 @@ function rollInspiration() {
 
 //input for manual rolls (like if you're using real dice). changes public variables directly.
 function rollManual(dNum, dSid, rolls) {
-    diNumbr = dNum;
-    diSides = dSid;
+    di.nmbr = dNum;
+    di.sids = dSid;
     //rolls needs to be an array.
-    diRolls = rolls;
-    diTotal = rolls.reduce(function(a ,b) {
-	return a + b;
-    });
+    di.rols = rolls;
+    di.totl = rolls.reduce(function(a ,b) { return a + b; });
     rollDBizer();
     wheatonize();
     return rollOut();
